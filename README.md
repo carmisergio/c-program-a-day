a8e019ce9fa22ba7e06bdf17aab7037bfc222248
Sergio Carmine <me@sergiocarmi.net>
2022-02-26 17:44:10 +0100

    Add build infrastructure and example program

--- README.md
+++ README.md
@@ -3,8 +3,9 @@
 Just like "a drawing every day" projects, but for people who can't draw.
 
 ## Rules
+
 - Every day, a small programming project written in c must be committed
 - The code has to be written in a single day
 - Old projects can not be modified after the day has passed
 - COMMENTS!
-- Before committing, the current project must be added to the shared makefile
+- Before committing, the current project must be added to the makefile
---
+++ sergio/helloworld.c
@@ -0,0 +1,12 @@
+/********************************************************************************
+ * Filename: helloworld.c
+ * Description: A simple program that prints "Hello, world!" on the screen
+ * Author: Sergio Carmine <me@sergiocarmi.net>
+ * Created: 26/02/2022
+********************************************************************************/
+#include <stdio.h>
+
+int main() {
+  printf("Hello, world!\n"); // Print "Hello, world!" to screen
+  return 0;
+}
