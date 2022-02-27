0698e71f24664e34d5fdfc43caaaa297faf32561
Sergio Carmine <me@sergiocarmi.net>
2022-02-27 23:39:24 +0100

    Add program of the day: excommunicdo.c

---
+++ sergio/excommunicado.c
@@ -0,0 +1,29 @@
+/********************************************************************************
+ * Filename: excommunicado.c
+ * Description: A Program that simulates sending a John Wick excommunicado.
+ * Author: Sergio Carmine <me@sergiocarmi.net>
+ * Created: 26/02/2022
+********************************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/********************************************************************************
+ * Prints random sending message with random numbers
+********************************************************************************/
+int send_messages() {
+  while(true) {
+    /// Print string
+    printf("]SENDING MESSAGE TO..... %03d-%03d-%04d\n]\n", rand() % 999, rand() % 999, rand() % 9999);
+    // Sleep a random amount of time between 450 and 550 milliseconds
+    sleep(1);
+  }
+}
+
+
+int main() {
+  // Start sending messages
+  send_messages();
+
+  return 0;
+}
